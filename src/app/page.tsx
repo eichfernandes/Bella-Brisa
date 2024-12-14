7ec9895f518928b0517de40e9c41303658667858
Rafael Eich <rrafael.fernandes@gmail.com>
2024-12-14 14:20:29 -0300

    Remoção da página inicial para que ela redirecione automaticamente para login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,31 +5,8 @@ import { useRouter } from 'next/navigation';
 
 const WelcomePage = () => {
   const router = useRouter();
-
-  const handleNavigation = () => {
-    router.push('/login'); // Navega para a página de Login
-  };
-
   return (
-    <div className="container" style={{ textAlign: 'center', marginTop: '50px' }}>
-      <h1>Bem-vindo</h1>
-      <p>Faça login para acessar o sistema.</p>
-      <button 
-        onClick={handleNavigation} 
-        className="inputButton" 
-        style={{
-          padding: '10px 20px',
-          fontSize: '16px',
-          cursor: 'pointer',
-          backgroundColor: '#0070f3',
-          color: '#fff',
-          border: 'none',
-          borderRadius: '5px'
-        }}
-      >
-        Ir para o Login
-      </button>
-    </div>
+    router.push('/login')
   );
 };
 
